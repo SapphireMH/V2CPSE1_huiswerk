a9821e2747f8860f51b060a7896635cd6e70ba17
Mike Hoogendoorn <thisismike97@me.com>
2019-09-11 16:40:22 +0200

    Update main.cpp

--- Week 1/Week 1: deel 4/main.cpp
+++ Week 1/Week 1: deel 4/main.cpp
@@ -11,17 +11,6 @@ void uart_put_char( char c ){
    hwlib::cout << c;
 }
 
-//char toggle_case( char c){
-//	
-//	if( c > 64 && c < 91 ){
-//		c += 32;
-//	}
-//	else if( c > 96 && c < 123 ){
-//		c -= 32;
-//	}
-//	return c;
-//}
-
 int main( void ){	
    
    namespace target = hwlib::target;   
